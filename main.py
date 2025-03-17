55b2b2d8dd2ce1fa3f05081fe588be9a0e457306
Drumitch <bravoalpha15tango@gmail.com>
2025-03-16 22:12:32 -0400

    0.0.3| add fps limit; add circleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     black = (0, 0, 0)
     while True:
         for event in pygame.event.get():
@@ -11,9 +15,10 @@ def main():
                 return
         screen.fill(black)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = game_clock.tick(60) / 1000
+print("Starting Asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
